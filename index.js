70471edde044a2e573d6b58b04386f31a1b2eb3b
Jarrett Lusso <jclusso@gmail.com>
2024-02-28 14:16:25 -0500

    Add ESLint

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  ignorePatterns: [
+    '**/vendor/**/*.js',
+  ],
+  rules: {
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'comma-dangle': 'error',
+    'curly': ['error', 'multi-line'],
+    'no-console': ['error', { 'allow': ['warn', 'error'] }],
+    'no-duplicate-imports': ['error'],
+    'no-multi-spaces': ['error'],
+    'no-multiple-empty-lines': ['error', { 'max': 2 }],
+    'no-self-assign': ['error', { 'props': false }],
+    'no-trailing-spaces': ['error'],
+    'no-unused-vars': ['error', {
+      'varsIgnorePattern': '^_',
+      'argsIgnorePattern': '^_'
+    }],
+    'no-var': ['error'],
+    'prefer-const': ['error'],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'eqeqeq': ['error', 'smart']
+  }
+}
